939893d27aee367847ca8131a4971fb2d02d1f73
思月行云 <kenkao_01@126.com>
2018-07-03 18:57:25 +0800

    1

--- components/component.go
+++ components/component.go
@@ -1,5 +1,11 @@
 package components
 
+import (
+	"github.com/itfantasy/gonode/components/mysql"
+	"github.com/itfantasy/gonode/components/rabbitmq"
+	"github.com/itfantasy/gonode/components/redis"
+)
+
 const (
 	Redis    string = "Redis"
 	MySql           = "MySql"
@@ -12,4 +18,16 @@ type IComponent interface {
 	Close()
 	SetAuther(string, string)
 	SetOption(string, string)
+}
+
+func NewRedis() *redis.Redis {
+	return redis.NewRedis()
+}
+
+func NewMySql() *mysql.MySql {
+	return mysql.NewMySql()
+}
+
+func NewRabbitMQ() *rabbitmq.RabbitMQ {
+	return rabbitmq.NewRabbitMQ()
 }
